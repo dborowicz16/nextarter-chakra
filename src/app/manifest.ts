db8a89714bd49cb33e12dd0d23bf68f0d85c2164
dborowicz16 <79418706+dborowicz16@users.noreply.github.com>
2024-03-25 14:06:51 -0400

    remvoed navbar

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -12,9 +12,9 @@ const manifest = (): MetadataRoute.Manifest => ({
   prefer_related_applications: false,
   icons: [
     {
-      src: '/favicon.ico',
+      src: '/blackDevices.png',
       purpose: 'any',
-      sizes: '48x48 72x72 96x96 128x128 256x256',
+      sizes: '16x16 48x48 72x72 96x96 128x128 256x256',
     },
   ],
 });
